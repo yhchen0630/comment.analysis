f7a83fe41909a373872093d7832e698866882d8e
Addison Chen <mracn0630@gmail.com>
2019-11-10 12:35:19 +0800

    3th version, still can't understand what's len(d) and len(data) different ?

--- comment.py
+++ comment.py
@@ -7,7 +7,16 @@ with open('reviews.txt', 'r')as f:
         count += 1
         if count % 100000 == 0:
             print(len(data))
-print(len(data))
+print('總共有', len(data), '筆資料')
 print(data[0])
 print('----------------------------------')
 print(data[1])
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('留言的平均長度', sum_len/len(d))
+print('留言的平均長度', sum_len/len(data))
+
+print(len(d))
+print(len(data))
