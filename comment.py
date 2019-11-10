622e20bfb527a0f68a98626de96fce17d1723802
Addison Chen <mracn0630@gmail.com>
2019-11-10 11:56:25 +0800

    2nd version with count

--- comment.py
+++ comment.py
@@ -1,9 +1,12 @@
 #讀取檔案
-
+count = 0
 data = []
 with open('reviews.txt', 'r')as f:
     for x in f:
         data.append(x.strip())
+        count += 1
+        if count % 100000 == 0:
+            print(len(data))
 print(len(data))
 print(data[0])
 print('----------------------------------')
